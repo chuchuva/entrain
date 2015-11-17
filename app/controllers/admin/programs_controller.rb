7819f6ca90e15cca95f47fb5196c2e2ea24fa368
Pavel Chuchuva <pavel@chuchuva.com>
2015-11-17 14:54:09 +1100

    Add price to Edit Program page

--- app/controllers/admin/programs_controller.rb
+++ app/controllers/admin/programs_controller.rb
@@ -54,6 +54,6 @@ class Admin::ProgramsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def program_params
-      params.require(:program).permit(:name, :slug, :content)
+      params.require(:program).permit(:name, :slug, :price, :content)
     end
 end
