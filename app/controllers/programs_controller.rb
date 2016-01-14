675bfb3edb13e7572acfe455399801cf298e1e02
Pavel Chuchuva <pavel@chuchuva.com>
2016-01-14 20:16:37 +1100

    Make program 6 default

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,6 +2,8 @@ class ProgramsController < ApplicationController
   before_action :logged_in_user
   def index
     program = @current_site.programs.first
+    
+    return redirect_to program_path(id: 6) if program.id == 5
     redirect_to program if program
   end
 
