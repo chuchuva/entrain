3791a5f1084cfce98a3cb3a55a4f462e24f3f0d8
Pavel Chuchuva <pavel@chuchuva.com>
2016-02-27 17:31:20 +1100

    Add existing users to program - instead of inviting

--- app/controllers/admin/invites_controller.rb
+++ app/controllers/admin/invites_controller.rb
@@ -18,6 +18,20 @@ class Admin::InvitesController < Admin::AdminController
   # POST /admin/programs/1/invites
   def create
     @invite = @program.invites.build(invite_params)
+    user = @current_site.find_user_by_email(@invite.email)
+    if user
+      if user.has_access?(@program)
+        @invite.errors[:base] <<
+          "This person already has access to program #{@program.name}."
+        return render :new
+      end
+      @program.add_user user
+      user.send_welcome_email(@program)
+      redirect_to admin_program_invites_url(@program),
+        notice: "Added #{user.email} to the program and sent welcome email."
+      return 
+    end
+
     @invite.site_id = @program.site_id
     @invite.invited_by = current_user
     if @invite.save
