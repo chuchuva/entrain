3108d1d275d996fefb06d25c7c70d3c56310013d
Pavel Chuchuva <pavel@chuchuva.com>
2016-04-09 07:41:56 +1000

    Allow new line in Markdown without 2 spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ require 'redcarpet'
 module ApplicationHelper
   def markdown(text)
     return nil if text.blank?
-    renderer = Redcarpet::Render::HTML.new(with_toc_data: true)
+    renderer = Redcarpet::Render::HTML.new(with_toc_data: true, hard_wrap: true)
     markdown = Redcarpet::Markdown.new(renderer, autolink: true)
     markdown.render(text).html_safe
   end
