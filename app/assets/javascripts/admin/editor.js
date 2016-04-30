c658f011c62be9a5bd86813e287cc031a651e80b
Pavel Chuchuva <pavel@chuchuva.com>
2016-04-30 17:06:00 +1000

    No need to bind to live events in DOM ready

--- app/assets/javascripts/admin/editor.js
+++ app/assets/javascripts/admin/editor.js
@@ -1,9 +1,7 @@
-$(function() {
-  $(document).on('show.bs.tab', '#preview-tab', function (e) {
-    $('#preview').html(marked($('#write textarea').val(),
-      {gfm: true, breaks: true}));
-  });
-  $(document).on('shown.bs.tab', '#write-tab', function (e) {
-    $('#write textarea').focus();
-  });
+$(document).on('show.bs.tab', '#preview-tab', function (e) {
+  $('#preview').html(marked($('#write textarea').val(),
+    {gfm: true, breaks: true}));
+});
+$(document).on('shown.bs.tab', '#write-tab', function (e) {
+  $('#write textarea').focus();
 });
